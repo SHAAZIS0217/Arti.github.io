54b48c5859113cc2dea57b563a81abf8f63136d7
SHAAZIS0217 <shaazisofficial02@gmail.com>
2024-08-13 12:58:30 +0530

    on main

---
+++ constants.js
@@ -0,0 +1,68 @@
+// Options the user could type in
+const prompts = [
+  ["how are you"],
+    ["hi", "hey", "hello", "good morning", "good afternoon","whatsup"],
+    ["date","when association","when association meeting?","date of the association meeting"],
+    ["what are you doing", "what is going on", "what is up"],
+    ["venue","Venue","where is venue","where is venue?","where venue","where is the event","where is the event?","Where is the event"],
+    ["who is ahod","who is ahod?","ahod","who is ahod for aids"],
+    ["Who is hod","hod","who is hod?","who is hod for department of aids"],
+    [
+      "cheif guest?","who is the cheif guest","can i know about cheif guest"
+    ],
+    ["event name?","Event name","Name of the event?","event"],
+    ["happy", "good", "fun", "wonderful", "fantastic", "cool"],
+    ["bad", "bored", "tired"],
+    ["help me", "tell me story", "tell me joke"],
+    ["ah", "yes", "ok", "okay", "nice"],
+    ["bye", "good bye", "goodbye", "see you later"],
+    ["what should i eat today"],
+    ["bro"],
+    ["what", "why", "how", "where", "when"],
+    ["no","not sure","maybe","no thanks"],
+    [""],
+    ["haha","ha","lol","hehe","funny","joke"]
+  ]
+  
+  // Possible responses, in corresponding order
+  
+  const replies = [
+    ["iam fine"],
+    ["Hello!", "Hi!", "Hey!", "Hi there!","Howdy"],
+    [
+      "16th august 2024"
+    ],
+    [
+      "Nothing much",
+      "About to go to sleep",
+      "I don't know actually"
+    ],
+    ["1st Floor Conference Hall"],
+    ["Mrs. V.Sankari & Ms. R.Dhaaraani"],
+    ["Dr. Kiran Bala"],
+    ["Swami Nithiyanandha"],
+    ["Intelligence Association Meeting"],
+    ["Have you ever felt bad?", "Glad to hear it"],
+    ["Why?", "Why? You shouldn't!", "Try watching TV"],``
+    ["What about?", "Once upon a time..."],
+    ["Tell me a story", "Tell me a joke", "Tell me about yourself"],
+    ["Bye", "Goodbye", "See you later"],
+    ["Sushi", "Pizza"],
+    ["whatsup Bro!"],
+    ["Great question"],
+    ["That's ok","I understand","What do you want to talk about?"],
+    ["Please say something :("],
+    ["Haha!","Good one!"]
+  ]
+  
+  // Random for any other user input
+  
+  const alternative = [
+    
+    "I don't understand ",
+    "can you repeat your question please"
+  ]
+  
+  // Whatever else you want :)
+  
+  const coronavirus = ["Please stay home", "Wear a mask", "Fortunately, I don't have COVID", "These are uncertain times"]
