5ab8a7812fd3519c4864cda18f10992c9c9b27ae
SHAAZIS0217 <shaazisofficial02@gmail.com>
2024-08-14 13:11:54 +0530

    Add files via upload

--- constants.js
+++ constants.js
@@ -67,3 +67,4 @@ const prompts = [
   
   const ed = ["Sir name is Dr. S.Kuppuswamy " , "Dr. S.Kuppuswamy" , "he is Dr. S.Kuppuswamy"];
   const princi = ["Sir name is Dr. D.Srinivasan" , "Dr. D.Srinivasan" , "he is Dr. D.Srinivasan"];
+const time = ["time is 10.00 AM","The event starts at 10.00 AM" , " its 10.00 AM , Hoping to see you there"];
